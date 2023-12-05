6a13570615f29c9c3a64b986ca238e7819f37fe1
Raphael Palladino <145732258+RayzoSix@users.noreply.github.com>
2023-12-05 17:13:27 -0500

    Add files via upload
    My first ever vue application. It is a game for creating an event with a button, and starting a timed click event game that logs how quickly you press the pop-up.

---
+++ src/components/Results-one.vue
@@ -0,0 +1,34 @@
+<template>
+<p> Reaction time - {{ score }} ms</p>
+<p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Speedstar'
+        } else if (this.rank < 400) {
+            this.rank = 'average gamer'
+        } else {
+            this.rank = 'could not be much worse'
+        }
+    }
+    
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: green;
+        font-weight: bold;
+    
+    }
+</style>
